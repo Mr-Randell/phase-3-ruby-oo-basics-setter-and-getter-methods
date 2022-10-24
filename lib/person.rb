7fa2272d29527a7c34839320d4b434beba945baa
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-24 22:48:06 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
+    # name methods
+    # setter
+    def name=(name)
+        @name = name
+    end
 
+    # getter
+    def name
+        @name
+    end
+
+    # job methods
+    # setter
+    def job=(job)
+        @job = job
+    end
+
+    # getter
+    def job
+        @job
+    end
 end
