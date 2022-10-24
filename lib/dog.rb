6b14f7f80d83d1a5e39c4b7c21a4bf2378b2b68b
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-24 22:47:44 +0300

    commit tree 6cd1bd9f1b323c26a2cc5104c8f1903cfe4d9e20

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+    # name methods
+    # setter
+    def name=(name)
+        @name = name
+    end
 
+    # getter
+    def name
+        @name
+    end
+
+    # breed methods
+    # setter
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # getter
+    def breed
+        @breed
+    end
 end
